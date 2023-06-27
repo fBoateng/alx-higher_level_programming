4c418a82df4158a8dc83f7ace23e856e7fa1c293
Francis Boateng <b0af@ymail.com>
2023-06-27 14:53:42 +0000

    Class stand up

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines class Square"""
+
+
+class Square:
+    """ Empty square class
+    without attribute or methods.
+    """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Definition of Square class"""
+
+
+class Square:
+    """Represent a square
+
+    Attributes:
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """Initializes a new square
+
+        Args:
+            size (int): size of a side of the square
+
+        
+        """
+        self.__size = size
